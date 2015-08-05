6fcde44d9158df9182d2393a637add5e2774699c
George Syrop <george@syrop.co>
2015-08-04 18:40:11 -0700

    got rid of font sizing

--- app.js
+++ app.js
@@ -547,7 +547,7 @@ var main = function () {
 				$(xml).find('etd').each(function(){
 					var abbr = $(this).find('abbreviation').text();
 					var destination = $(this).find('destination').text();
-					$("<div class='row "+ abbr +"' style="fontsize: 2.0rem"></div>").html(destination).appendTo('#station-label');
+					$("<div class='row "+ abbr +"'></div>").html(destination).appendTo('#station-label');
 					$(this).find('estimate').each(function(){
 						var minutes = $(this).find('minutes').text();
 						var length = $(this).find('length').text();
