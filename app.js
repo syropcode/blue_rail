52728c2ce77c6e73019d2ee3937e9c3a7d9abdbc
George Syrop <george@syrop.co>
2015-08-04 23:26:25 -0700

    updated skeleton spacing

--- app.js
+++ app.js
@@ -556,11 +556,11 @@ var main = function () {
 						if (minutes === "Leaving") {	
 						}
 						else if (minutes === "Arriving") {
-							$('<h5 class="one column"></h5>').text(minutes+"   "+length+" cars").appendTo('.'+abbr);	
+							$('<h5 class="two columns"></h5>').text(minutes+"   "+length+" cars").appendTo('.'+abbr);	
 						}
 						else {
-							$('<h5 class="one column"></h5>').text(minutes+"m").appendTo('.'+abbr);
-							$('<h5 class="one column"></h5>').text(length+" cars").appendTo('.'+abbr);	
+							$('<h5 class="two columns"></h5>').text(minutes+"m").appendTo('.'+abbr);
+							$('<h5 class="two columns"></h5>').text(length+" cars").appendTo('.'+abbr);	
 						}
 						
 					});
