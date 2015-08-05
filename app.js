381a6856d2935756a9512345229649f47e37723e
George Syrop <george@syrop.co>
2015-08-04 18:23:50 -0700

    updated font sizing, wait logic

--- app.js
+++ app.js
@@ -547,19 +547,18 @@ var main = function () {
 				$(xml).find('etd').each(function(){
 					var abbr = $(this).find('abbreviation').text();
 					var destination = $(this).find('destination').text();
-					$("<div class='row "+ abbr +"'></div>").html(destination).appendTo('#station-label');
+					$("<div id='line' class='row "+ abbr +"'></div>").html(destination).appendTo('#station-label');
 					$(this).find('estimate').each(function(){
 						var minutes = $(this).find('minutes').text();
 						var length = $(this).find('length').text();
 						var color = $(this).find('hexcolor').text();
-						if (minutes === "Leaving") {
-							$('<h5></h5>').text(minutes).appendTo('.'+abbr);	
+						if (minutes === "Leaving") {	
 						}
 						else if (minutes === "Arriving") {
-							$('<h5></h5>').text(minutes+"   "+length+" cars").appendTo('.'+abbr);	
+							$('<h5></h5>').text(minutes+"&nbsp"+length+" cars").appendTo('.'+abbr);	
 						}
 						else {
-							$('<h5></h5>').text(minutes+"m   " +length+" cars").appendTo('.'+abbr);	
+							$('<h5></h5>').text(minutes+"m"+"&nbsp"+length+" cars").appendTo('.'+abbr);	
 						}
 						
 					});
